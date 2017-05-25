636a5d5908a148895c21bf6f62395f9bb1f7798d
zero-FTW <w314877761@hotmail.com>
2017-05-25 00:29:34 -0400

    1A

---
+++ Codeforces/1A.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+
+public class JavaApplication1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);  
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        int column, row = 0;
+        if (a > b) {
+            row = (b + c - 1) / c;
+            column = (a + c -1) / c;
+        }
+        else if (a < b) {
+            row = (a + c -1) / c;
+            column = (b + c - 1) / c;   
+        }
+        else {
+            column = (b + c - 1) / c;
+            row = column;
+        }
+        long result = (long) row * (long) column;
+        System.out.println(result);
+    }
+}
