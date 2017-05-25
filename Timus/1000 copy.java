e6de642804f37fef5202f178b56703a3bf10581d
zero-FTW <w314877761@hotmail.com>
2017-05-24 23:40:05 -0400

    This is the summary
    This is the description

---
+++ Timus/1000 copy.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+
+public class JavaApplication1 {
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+        int b = in.nextInt();
+        System.out.println(a + b);
+    }
+}
