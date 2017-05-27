533cbd996142ac04fab8ea9b5e14562b02979c38
zero-FTW <w314877761@hotmail.com>
2017-05-26 21:23:24 -0400

    1068

--- Timus/1068.java
+++ Timus/1068.java
@@ -1,10 +1,6 @@
 import java.util.Scanner;
 
 public class JavaApplication1 {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);  
         int a = in.nextInt();
